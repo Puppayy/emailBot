3756be1325e6b69bef94a2fcafa4025c8a378621
Matthew Groholski <mattgroholski@yahoo.com>
2022-03-27 12:26:05 -0700

    Seperated files

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ else:
     exit()
 
 
-""" emailAmount = input("Please input the amount of emails you would like to enter: ")
+emailAmount = input("Please input the amount of emails you would like to enter: ")
 
 try:
     emailAmount = int(emailAmount)
@@ -21,29 +21,19 @@ except:
 emails = []
 for i in range(0,emailAmount):
     email = input("Please input email " + i)
-    emails.append(email) """
+    emails.append(email)
 
+    #gets message
+    subject = input("Input Subject Line: ")
+    body = input("Input Body: ")
 
+    message = "Subject" + subject + " \n\n " + body
+    with smtplib.SMTP('smtp.gmail.com', 587) as server: 
+        server.starttls()
+        server.login(sender_email, password)
+        for receiver_email in emails:
+            server.sendmail(sender_email, receiver_email, message)
 
-receiver_email1 = "mattgroholski@yahoo.com"
-receiver_email2 = "candersen6416@gmail.com"
-
-currentTime = datetime.datetime.now()
-
-while not (currentTime.hour == "24"):
-    if (currentTime.minute == "00"):
-        #gets stock price
-        voo = yfinance.Ticker("VOO")
-        message = "Subject: VOO Stock Price \n\n The current VOO stock price is: " + voo.info["regularMarketPrice"]
-        with smtplib.SMTP('smtp.gmail.com', 587) as server: 
-            server.starttls()
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email1, message)
-            server.sendmail(sender_email, receiver_email2, message)
-            """ for receiver_email in emails:
-                server.sendmail(sender_email, receiver_email, message) """
-
-    currentTime = datetime.datetime.now()
 
 print("Program Ending")
 
